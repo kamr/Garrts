10606bd0bcf96c997e07ea93dd365d4e604f519a
zzzz <garreett@hotmail.co.uk>
2016-07-10 23:39:20 +0100

    camera, selection, networking

---
+++ Assets/IDK.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IDK : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnPlayerConnected(NetworkPlayer player) {
+		print ("ggggggggggggggggggggggggggg");
+	}
+}
---
+++ Assets/spawnUnits.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class spawnUnits : MonoBehaviour {
+	ArrayList allPlayers = new ArrayList();
+	// Use this for initialization
+	void Start () {
+		print ("ggg");
+
+		//NetworkServer.SpawnWithClientAuthority (g, Network.connections [0].);
+	}
+	void OnPlayerConnected(NetworkPlayer player) {
+		print ("asdasdafsa");
+		/*GameObject networkedPlayerObj = new GameObject();
+		allPlayers.Add (networkedPlayerObj);
+
+		GameObject g = Resources.Load ("unit1") as GameObject;
+
+		NetworkServer.SpawnWithClientAuthority (g,networkedPlayerObj);
+		NetworkServer.SpawnWithClientAuthority (g,networkedPlayerObj);
+*/
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
