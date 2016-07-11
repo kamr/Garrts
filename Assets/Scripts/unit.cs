7d4ed6ef7c1cf9793c3febaab5fde7f4b506a90f
Kamran <kamran.khan@magd.ox.ac.uk>
2016-07-11 14:46:56 +0100

    Draw Selection Box

---
+++ Assets/Scripts/unit.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class unit : NetworkBehaviour {
+	public GameObject circle;
+
+	bool selected = false;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent <NavMeshAgent> ();
+	}
+
+	public void Select() {
+		selected = true;
+		print ("Unit " + this.name + " selected!");
+		gameObject.GetComponent<Renderer> ().material.SetColor ("_Color", Color.red);
+	}
+
+	public void UnSelect() {
+		selected = false;
+		gameObject.GetComponent<Renderer> ().material.SetColor ("_Color", Color.white);
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(1) && selected) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				print (hit.point);
+				agent.SetDestination (hit.point);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/unitCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unitCreator : MonoBehaviour {
+
+
+	void OnPlayerConnected(NetworkPlayer player) {
+		print ("asdasdafsa");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
